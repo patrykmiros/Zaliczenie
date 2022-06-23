ee01e39ee9a070dcd8a3f6917cdf7944c1ac715a
patryk <patryk.miros111@gmail.com>
2022-06-23 17:57:42 +0200

    dodanie 2 plikow

---
+++ plikdrugi222/src/main.java
@@ -0,0 +1,14 @@
+public class glowna {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("drugiplikj");
+        System.out.println("drugiplikj");
+        System.out.println("drugiplikj");
+        System.out.println("drugiplikj");
+
+
+    }
+}
---
+++ untitled1/src/glowna.java
@@ -0,0 +1,9 @@
+public class glowna {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("costam");
+    }
+}
